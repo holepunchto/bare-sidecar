bb598de15dacb7b62a2e6c3ee8a4c472cab96c59
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-02-14 21:39:21 +0100

    Leave IPC ref'ed by default

--- test.js
+++ test.js
@@ -4,11 +4,18 @@ const Sidecar = require('bare-sidecar')
 const isWindows = Bare.platform === 'win32'
 
 test('basic', (t) => {
-  t.plan(1)
+  t.plan(3)
 
   const sidecar = new Sidecar(require.resolve('./test/fixtures/echo'))
 
-  sidecar.on('data', (data) => t.is(data.toString(), 'Hello world')).write('Hello world')
+  sidecar
+    .on('exit', () => t.pass('exited'))
+    .on('close', () => t.pass('closed'))
+    .on('data', (data) => {
+      t.is(data.toString(), 'Hello world')
+      sidecar.destroy()
+    })
+    .write('Hello world')
 })
 
 test('kill on destroy', (t) => {
@@ -21,9 +28,7 @@ test('kill on destroy', (t) => {
       t.is(code, isWindows ? 1 : 0)
       t.is(status, 15)
     })
-    .on('close', () => {
-      t.pass('closed')
-    })
+    .on('close', () => t.pass('closed'))
     .destroy()
 })
 
@@ -37,9 +42,7 @@ test('exit on ipc close', (t) => {
       t.is(code, 0)
       t.is(status, 0)
     })
-    .on('close', () => {
-      t.pass('closed')
-    })
+    .on('close', () => t.pass('closed'))
 })
 
 test('exit on ipc destroy', (t) => {
@@ -52,9 +55,7 @@ test('exit on ipc destroy', (t) => {
       t.is(code, 0)
       t.is(status, 0)
     })
-    .on('close', () => {
-      t.pass('closed')
-    })
+    .on('close', () => t.pass('closed'))
 })
 
 test('uncaught throw', { skip: isWindows }, (t) => {
@@ -67,7 +68,5 @@ test('uncaught throw', { skip: isWindows }, (t) => {
       t.is(code, isWindows ? 3 : 0)
       t.is(status, isWindows ? 0 : 6)
     })
-    .on('close', () => {
-      t.pass('closed')
-    })
+    .on('close', () => t.pass('closed'))
 })
